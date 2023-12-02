af1dc19fa3ca9a043097fdc8eff2b0569e06de59
Bruce V Wilcox <brucevwilcox@itips.info>
2023-12-02 15:23:01 -0700

    2023-12-02 15:23

--- src/main/java/org/taxdataexchange/usage/tax1098e/Tax1098EDocumentGenerator.java
+++ src/main/java/org/taxdataexchange/usage/tax1098e/Tax1098EDocumentGenerator.java
@@ -56,7 +56,7 @@ public class Tax1098EDocumentGenerator {
         Tax1098EPdfBuilder pdfBuilder = new Tax1098EPdfBuilder( );
         byte[] bytes = pdfBuilder.buildQr( taxDataList );
 
-        String filePath = "samples/Tax1098E.sample.png";
+        String filePath = "samples/Tax1098E.qr.png";
         FileUtils.bytesToFile( bytes, filePath );
         System.out.println( filePath );
 
@@ -84,6 +84,12 @@ public class Tax1098EDocumentGenerator {
         String filePath = "samples/Tax1098E.sample.pdf";
         FileUtils.bytesToFile( pdfBytes, filePath );
         System.out.println( filePath );
+
+        String filePathPng = "samples/Tax1098E.sample.png";
+        byte[] pngBytes = Pdf2PngConverter.convertBytes( pdfBytes );
+        FileUtils.bytesToFile( pngBytes, filePathPng );
+        System.out.println( filePathPng );
+
 
     }
 
